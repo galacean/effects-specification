c645cadba5b941b9779e9b4158ead25d8407d736
wumaolinmaoan <wumaolin1998@outlook.com>
2024-03-27 15:14:20 +0800

    feat: add TimelineComponent data

--- src/item/null-item.ts
+++ src/item/null-item.ts
@@ -7,6 +7,7 @@ import type {
 } from '../type';
 import type { RGBAColorValue } from '../numberExpression';
 import type { BaseItem } from './base-item';
+import { ComponentData, EffectsObjectData } from 'src/components';
 /**
  * 空节点元素
  */
@@ -50,4 +51,48 @@ export interface NullContent {
    * 空节点元素色彩变化属性
    */
   colorOverLifetime?: ColorOverLifetime,
+}
+
+export interface TimelineComponentData extends ComponentData {
+  /**
+   * 轨道数据
+   */
+  tracks: TrackData[];
+}
+
+export interface TrackData {
+  type: string,
+  clips: TimelineClipData[],
+}
+
+export interface TimelineClipData {
+  start: number,
+  duration: number,
+  asset: EffectsObjectData,
+}
+
+export interface TransformAnimationClipData extends EffectsObjectData {
+  /**
+   * 空节点元素大小变化属性
+   */
+  sizeOverLifetime?: SizeOverLifetime,
+  /**
+   * 空节点元素旋转变化属性
+   */
+  rotationOverLifetime?: RotationOverLifetime,
+  /**
+   * 空节点元素位置变化属性
+   */
+  positionOverLifetime?: PositionOverLifetime,
+}
+
+export interface SpriteColorAnimationClipData extends EffectsObjectData {
+  /**
+   * 空节点元素色彩变化属性
+   */
+  colorOverLifetime?: ColorOverLifetime,
+  /**
+   * 图层初始颜色
+   */
+  startColor?: RGBAColorValue,
 }
