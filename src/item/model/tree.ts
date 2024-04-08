5ca457e46435d61ee3a1e210d80afc402d5d243d
liuxi150 <liuxi150@hotmail.com>
2024-04-08 14:27:56 +0800

    fix: add animation data

--- src/item/model/tree.ts
+++ src/item/model/tree.ts
@@ -2,6 +2,7 @@ import type { BinaryEnv } from '../../binary';
 import type { ModelAnimationTrackDataPointer } from './binary';
 import type { BaseItem, BaseItemTransform } from '../base-item';
 import type { SizeOverLifetime, RotationOverLifetime, PositionOverLifetime, ItemType } from '../../type';
+import type { ComponentData, DataPath } from '../../components';
 
 export interface TreeNodeOptions {
   name?: string,
@@ -69,4 +70,28 @@ export interface ModelTreeContent<T extends BinaryEnv> {
 export interface ModelTreeItem<T extends BinaryEnv> extends BaseItem {
   type: ItemType.tree,
   content: ModelTreeContent<T>,
+}
+
+export interface ModelAnimTrackData {
+  item: DataPath,
+  input: number[],
+  output: number[],
+  path: 'translation' | 'rotation' | 'scale' | 'weights',
+  interpolation: 'LINEAR' | 'STEP' | 'CUBICSPLINE',
+}
+
+export interface ModelAnimationData {
+  name?: string,
+  tracks: ModelAnimTrackData[],
+}
+
+export interface ModelAnimationComponentData extends ComponentData {
+  /**
+   * 默认动画索引，-1表示不播放动画
+   */
+  animation?: number,
+  /**
+   * 3D场景树中所有的动画数据
+   */
+  animations: ModelAnimationData[],
 }
