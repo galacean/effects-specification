b29ff0cb98f8a3e5f5f5638249f83bd5e71898cd
Sruim <934274351@qq.com>
2024-04-10 11:13:14 +0800

    refractor: 3d 数据 EC 化 (#18)
    * fix: update 3d spec
    
    * fix: update data type
    
    * fix: add animation data
    
    * chore: rename
    
    * fix: add orthographic camera
    
    * feat: 增加`ComponentData`的继承关系
    
    * perf: 优化代码
    
    * feat: 增加`ComponentData`继承关系
    
    * feat: 增加`VFXItemData`导出
    
    ---------
    
    Co-authored-by: liuxi150 <liuxi150@hotmail.com>
    Co-authored-by: 云垣 <suruimeng.srm@antgroup.com>

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ export * from './item/spine-item';
 export * from './item/filter-item';
 export * from './item/text-item';
 export * from './item/model';
+export * from './vfx-item-data';
 export * from './binary';
 export * from './text';
 export * from './components';
