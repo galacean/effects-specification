0de236753c9e84ead540b535a0a54b161a677bf6
wumaolinmaoan <wumaolin1998@outlook.com>
2024-04-23 15:19:31 +0800

    perf: opt code

--- src/buitin-object-guid.ts
+++ src/buitin-object-guid.ts
@@ -1,4 +1,4 @@
-export class BuiltinObjectGuid {
+export class BuiltinObjectGUID {
   static readonly WhiteTexture = 'whitetexture00000000000000000000';
   static readonly PBRShader = 'pbr00000000000000000000000000000';
   static readonly UnlitShader = 'unlit000000000000000000000000000';
