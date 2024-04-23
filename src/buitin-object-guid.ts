6c918909e8e14f98dc658bd6bd8970c61b745102
wumaolinmaoan <wumaolin1998@outlook.com>
2024-04-23 15:14:46 +0800

    feat: add 3d guid

--- src/buitin-object-guid.ts
+++ src/buitin-object-guid.ts
@@ -1,3 +1,5 @@
 export class BuiltInObjectGuid {
   static readonly WhiteTexture = 'whitetexture00000000000000000000';
+  static readonly PBRShader = 'pbr00000000000000000000000000000';
+  static readonly UnlitShader = 'unlit000000000000000000000000000';
 }
