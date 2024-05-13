749730b8d12acfa92ddbf694861b9ef12dd5f1b5
飂兮 <151803898+liuxi150@users.noreply.github.com>
2024-05-13 15:54:53 +0800

    fix: update vertex buffer semantic (#52)
    * feat: update vertex semantic
    
    * fix: add blend shape semantic
    
    ---------
    
    Co-authored-by: wumaolinmaoan <wumaolin1998@outlook.com>

--- src/components.ts
+++ src/components.ts
@@ -155,15 +155,45 @@ export interface VertexChannel {
   normalize?: boolean,
 }
 
+// BINORMAL[n]	Binormal	float4
+// BLENDINDICES[n]	混合索引	uint
+// BLENDWEIGHT[n]	混合权重	FLOAT
+// COLOR[n]	漫射和反射颜色	float4
+// NORMAL[n]	法向矢量	float4
+// POSITION[n]	对象空间中的顶点位置。	float4
+// POSITIONT	变换的顶点位置。	float4
+// PSIZE[n]	点大小	FLOAT
+// TANGENT[n]	正切	float4
+// TEXCOORD[n]	纹理坐标	float4
+// POSITION_BS[n]	Blend Shape 空间中的顶点位置	float4
+// NORMAL_BS[n]	Blend Shape 空间中的法向矢量	float4
+// TANGENT_BS[n]	Blend Shape 空间中的正切矢量	float4
 export enum VertexBufferSemantic {
-  Positon = 'position',
-  Uv = 'uv',
-  Uv2 = 'uv2',
-  Normal = 'normal',
-  Tangent = 'tangent',
-  Color = 'color',
-  BoneIndices = 'boneIndices',
-  BoneWeights = 'boneWeights'
+  Positon = 'POSITION',
+  Uv = 'TEXCOORD0',
+  Uv2 = 'TEXCOORD1',
+  Normal = 'NORMAL',
+  Tangent = 'TANGENT',
+  Color = 'COLOR',
+  Joints = 'JOINTS',
+  Weights = 'WEIGHTS',
+  //
+  PositionBS0 = 'POSITION_BS0',
+  PositionBS1 = 'POSITION_BS1',
+  PositionBS2 = 'POSITION_BS2',
+  PositionBS3 = 'POSITION_BS3',
+  PositionBS4 = 'POSITION_BS4',
+  PositionBS5 = 'POSITION_BS5',
+  PositionBS6 = 'POSITION_BS6',
+  PositionBS7 = 'POSITION_BS7',
+  NormalBS0 = 'NORMAL_BS0',
+  NormalBS1 = 'NORMAL_BS1',
+  NormalBS2 = 'NORMAL_BS2',
+  NormalBS3 = 'NORMAL_BS3',
+  TangentBS0 = 'TANGENT_BS0',
+  TangentBS1 = 'TANGENT_BS1',
+  TangentBS2 = 'TANGENT_BS2',
+  TangentBS3 = 'TANGENT_BS3',
 }
 
 export interface ShaderData extends EffectsObjectData {
