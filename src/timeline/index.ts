af48e629df2aad71f5783e78d9448cfd2d4da107
wumaolinmaoan <wumaolin1998@outlook.com>
2024-05-28 15:28:13 +0800

    perf: rename

--- src/timeline/index.ts
+++ src/timeline/index.ts
@@ -2,4 +2,4 @@ export * from './track/track-asset-data';
 export * from './sprite-color-playable-asset-data';
 export * from './timeline-asset-data';
 export * from './timeline-clip-data';
-export * from './transform-animation-playable-asset-data';
+export * from './transform-playable-asset-data';
