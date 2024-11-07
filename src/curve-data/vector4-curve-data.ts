776d82f3786ec6aff081de882d34d4bc2bae4a22
wumaolin.wml <wumaolin.wml@antgroup.com>
2024-11-07 11:53:58 +0800

    feat: add color and vector4 curve value

--- src/curve-data/vector4-curve-data.ts
+++ src/curve-data/vector4-curve-data.ts
@@ -1,8 +1,8 @@
 import type { BezierValue } from '../number-expression';
 
-export interface Vector4CurveData {
+export type Vector4CurveData = [
   x: BezierValue,
   y: BezierValue,
   z: BezierValue,
   w: BezierValue,
-}
+];
