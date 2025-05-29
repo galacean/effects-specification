9371e6b21330fd967eb02c7bedf9c37b6527da07
Yu <yuufen@qq.com>
2025-05-29 14:21:15 +0800

    feat: 补充参数

--- src/components/ffd-data.ts
+++ src/components/ffd-data.ts
@@ -2,5 +2,7 @@ import { Vector3Data } from 'src/math';
 import type { ComponentData } from './component-data';
 
 export interface FFDComponentData extends ComponentData {
-  controlPoints?: Vector3Data[],
+  controlPoints: Vector3Data[],
+  boundMin: Vector3Data,
+  boundMax: Vector3Data,
 }
