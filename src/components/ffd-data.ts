cdc5f7a3dc89650e61fecd67558306099b793f75
澄弈 <chengyi.zzm@gmail.com>
2025-08-06 14:55:30 +0800

    feat: add ffd data

---
+++ src/components/ffd-data.ts
@@ -0,0 +1,12 @@
+import type { Vector3Data } from 'src/math';
+import type { ComponentData } from './component-data';
+
+export interface FFDComponentData extends ComponentData {
+  controlPoints: Vector3Data[],
+  boundMin: Vector3Data,
+  boundMax: Vector3Data,
+  rest: boolean,
+  additive: boolean,
+  rowNum: number,
+  colNum: number,
+}
